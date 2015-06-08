b8dee8388a7d136c4e76b25ca6df8d739ab40ab4
Hossam Hazem <hossamhazem94@gmail.com>
2015-06-08 21:36:14 +0200

    Initial Output

---
+++ wizard.js
@@ -0,0 +1,142 @@
+
+	var Hour;
+	var Minute;
+	var Second;
+	var Month;
+	var Day;
+	var DayName;
+	var init=true;
+$(document).ready(function(){
+	$('.Stick').addClass('Hidden');
+	updatewatch();
+
+	
+
+
+})
+function updatewatch(){
+	if(init){
+	var currentdate = new Date(); 
+	Hour =currentdate.getHours();
+	Minute=currentdate.getMinutes();
+	Second=currentdate.getSeconds();
+	Month=currentdate.getMonth()+1;
+	Day = currentdate.getDate();
+	DayName=currentdate.getDay();
+	setSticks('HourOne',parseInt(Hour/10));
+	setSticks('HourTwo',Hour%10);
+	setSticks('MinuteOne',parseInt(Minute/10));
+	setSticks('MinuteTwo',Minute%10);
+	setSticks('SecondOne',parseInt(Second/10));
+	setSticks('SecondTwo',Second%10);
+	setSticks('DateMonthOne',parseInt(Month/10));
+	setSticks('DateMonthTwo',Month%10);
+	setSticks('DateDayOne',parseInt(Day/10));
+	setSticks('DateDayTwo',Day%10);
+	setTimeout(function(){unsetAll();updatewatch();}, Date.now()%1000);
+	}
+}
+function setSticks(parent,value){
+		if(value==0){
+		setStick(parent,'0');
+		setStick(parent,'1');
+		setStick(parent,'2');
+		setStick(parent,'4');
+		setStick(parent,'5');
+		setStick(parent,'6');
+	}
+	if(value==1){
+		setStick(parent,'2');
+		setStick(parent,'5');
+	}
+	if(value==2){
+		setStick(parent,'0');
+		setStick(parent,'2');
+		setStick(parent,'3');
+		setStick(parent,'4');
+		setStick(parent,'6');
+	}
+	if(value==3){
+		setStick(parent,'0');
+		setStick(parent,'2');
+		setStick(parent,'3');
+		setStick(parent,'5');
+		setStick(parent,'6');
+	}
+	if(value==4){
+		setStick(parent,'1');
+		setStick(parent,'2');
+		setStick(parent,'3');
+		setStick(parent,'5');
+	}
+	if(value==5){
+		setStick(parent,'0');
+		setStick(parent,'1');
+		setStick(parent,'3');
+		setStick(parent,'5');
+		setStick(parent,'6');
+	}
+	if(value==6){
+		setStick(parent,'0');
+		setStick(parent,'1');
+		setStick(parent,'3');
+		setStick(parent,'4');
+		setStick(parent,'5');
+		setStick(parent,'6');
+	}
+	if(value==7){
+		setStick(parent,'0');
+		setStick(parent,'2');
+		setStick(parent,'5');
+	}
+	if(value==8){
+		setStick(parent,'0');
+		setStick(parent,'1');
+		setStick(parent,'2');
+		setStick(parent,'3');
+		setStick(parent,'4');
+		setStick(parent,'5');
+		setStick(parent,'6');
+	}
+	if(value==9){
+		setStick(parent,'0');
+		setStick(parent,'1');
+		setStick(parent,'2');
+		setStick(parent,'3');
+		setStick(parent,'5');
+		setStick(parent,'6');
+	}
+		
+
+}
+function unsetAll(){
+	unsetSticks('DayOne');
+	unsetSticks('DayTwo');
+	unsetSticks('DayThree');
+	unsetSticks('DateDayOne');
+	unsetSticks('DateDayTwo');
+	unsetSticks('DateMonthOne');
+	unsetSticks('DateMonthTwo');
+	unsetSticks('HourOne');
+	unsetSticks('HourTwo');
+	unsetSticks('MinuteOne');
+	unsetSticks('MinuteTwo');
+	unsetSticks('SecondOne');
+	unsetSticks('SecondTwo');
+}
+function unsetSticks(parent){
+		unsetStick(parent,'0');
+		unsetStick(parent,'1');
+		unsetStick(parent,'2');
+		unsetStick(parent,'3');
+		unsetStick(parent,'4');
+		unsetStick(parent,'5');
+		unsetStick(parent,'6');
+}
+function setStick(parent,id){
+	$('#'+parent+'>#s'+id).removeClass('Hidden')
+}
+function unsetStick(parent,id){
+	$('#'+parent+'>#s'+id).addClass('Hidden')
+}
+
